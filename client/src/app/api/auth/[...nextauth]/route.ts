388582b32c2b28a7056e9f96f2e1bc8f7127d2da
Joey Clapton <joeyclapton42@gmail.com>
2024-04-15 14:41:48 -0300

    feat: create auth router

---
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,37 @@
+import LoginService from "@/app/shared/services/login";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const nextAuthOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        const url = "https://661d62bd98427bbbef01aea6.mockapi.io/api/v1/users";
+        const response = await fetch(url, {
+          method: "GET",
+        });
+        const users = await response.json();
+        const userByLogin = users?.filter((user) => {
+          return user.email === credentials?.email || user.password === credentials?.password;
+        });
+
+        if (userByLogin) return userByLogin;
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+};
+
+const handler = NextAuth(nextAuthOptions);
+
+export { handler as GET, handler as POST };
