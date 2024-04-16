4e74fbe4c37996895b01b6cc9ab9c2ad082bda5a
Joey Clapton <joeyclapton42@gmail.com>
2024-04-15 22:37:53 -0300

    feat: adjust home layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Login from "@/components/Login";
+import Login from "@/app/(auth-routes)/Login";
 import { useAuth } from "../app/shared/context/AuthContext";
-import HomeBack from "@/components/HomeBack";
+import HomeBack from "@/app/components/MenuBar2";
 
 export default function Home() {
   return (
-    <section className="flex justify-center items-center h-screen">
+    <section className="flex h-screen items-center justify-center">
       <Login />
     </section>
   );
