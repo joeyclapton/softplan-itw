0e99ca71bd96a4ac0a485221a2c8d6575bac8a21
Joey Clapton <joeyclapton42@gmail.com>
2024-04-16 14:40:02 -0300

    style: adjust menubar layout

--- client/src/app/components/MenuBar/index.tsx
+++ client/src/app/components/MenuBar/index.tsx
@@ -8,7 +8,7 @@ import { signOut } from "next-auth/react";
 const MenuBar = (user) => {
   const router = useRouter();
   const isAdmin = user?.user?.isAdmin;
-  console.log(user?.user);
+
   const onLogout = async () => {
     await signOut({
       redirect: false,
@@ -27,7 +27,7 @@ const MenuBar = (user) => {
               className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
             >
               <svg
-                className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-black dark:group-hover:text-black"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -44,7 +44,7 @@ const MenuBar = (user) => {
                 <button
                   data-tooltip-target="tooltip-new"
                   type="button"
-                  className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+                  className="inline-flex items-center justify-center w-10 h-10 font-medium g-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
                 >
                   <svg
                     className="w-4 h-4 text-white"
@@ -81,7 +81,7 @@ const MenuBar = (user) => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-black dark:group-hover:text-blue-500"
             >
               <path
                 fill-rule="evenodd"
